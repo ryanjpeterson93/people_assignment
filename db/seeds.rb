0487d83a45081c7f7a33db27bb7f70d4f2155ddf
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-02-19 16:01:13 -0700

    added faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# first_name, last_name, age, hair_color, eye_color, gender, alive
+
+
+require 'faker'
+
+
+10.times do
+  Person.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number.within(range: 18..75),
+    hair_color: Faker::Color.color_name,
+    eye_color: Faker::Color.color_name,
+    gender: Faker::Gender.binary_type,
+    alive: Faker::Boolean.boolean
+  )
+end
