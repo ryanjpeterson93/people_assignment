31c97be5dd1ff413a1e8ec4a81477929f546dbbc
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-02-19 17:38:45 -0700

    finished basic objectives

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,7 +13,7 @@ class PeopleController < ApplicationController
   end
 
   def create
-    @person = Person.new(pages_params)
+    @person = Person.new(people_params)
  
     if @person.save
       redirect_to people_path
@@ -22,6 +22,25 @@ class PeopleController < ApplicationController
     end
   end
  
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+  
+    if @person.update(people_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_path
+  end
+
   private
  
     def people_params
